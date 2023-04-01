1c9fecff9799f41a028756dbe0e7a60e2e4d195d
Claudio Cordova <claudio_cordova@yahoo.com>
2023-03-31 17:34:49 -0700

    Added EC2

--- create-stack.sh
+++ create-stack.sh
@@ -1,22 +1,34 @@
 
 
 
+if [ -z "$1" ]; then
+      MODE=FARGATE
+elif [ "$1" == "EC2" ]; then
+      MODE=$1
+elif [ "$1" == "FARGATE" ]; then
+      MODE=$1
+else
+    echo "Wrong parameter 1 MODE: "$1
+    exit 1 
+fi
+
+
 accounId=$(aws sts get-caller-identity --query Account --output text)
-region=$(aws configure get region)
-taskDefinitionArn="arn:aws:ecs:$region:$accounId:task-definition/poker-analyzer-service-task-definition:1"
+REGION=$(aws configure get region)
+taskDefinitionArn="arn:aws:ecs:$REGION:$accounId:task-definition/poker-analyzer-service-task-definition:1"
 containerName="poker-analyzer-service-container"
 
 ./generate_appspec_yaml.sh $taskDefinitionArn $containerName
-aws ecr get-login-password --region $region | docker login --username AWS --password-stdin 361494667617.dkr.ecr.us-east-1.amazonaws.com/amazoncorretto:8  
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin 361494667617.dkr.ecr.us-east-1.amazonaws.com/amazoncorretto:8  
 mvn clean package
 rm appspec.yaml
 
 
-aws cloudformation delete-stack --region $region --stack-name ecs-ecr-repository-stack
-aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-ecr-repository-stack
-aws cloudformation create-stack --region $region --stack-name ecs-ecr-repository-stack --template-body file://./ecs-ecr-repository.yaml  --parameters ParameterKey=RepositoryName,ParameterValue=poker-analyzer-service-repository --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation wait stack-create-complete --region $region --stack-name ecs-ecr-repository-stack
-repositoryUri=$(aws cloudformation describe-stacks --region $region --stack-name ecs-ecr-repository-stack | jq -r '.Stacks[0].Outputs[0].OutputValue')
+aws cloudformation delete-stack --region $REGION --stack-name ecs-ecr-repository-stack
+aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-ecr-repository-stack
+aws cloudformation create-stack --region $REGION --stack-name ecs-ecr-repository-stack --template-body file://./ecs-ecr-repository.yaml  --parameters ParameterKey=RepositoryName,ParameterValue=poker-analyzer-service-repository --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-ecr-repository-stack
+repositoryUri=$(aws cloudformation describe-stacks --region $REGION --stack-name ecs-ecr-repository-stack | jq -r '.Stacks[0].Outputs[0].OutputValue')
 
 
 # Delete ECR Repository
@@ -25,12 +37,16 @@ repositoryUri=$(aws cloudformation describe-stacks --region $region --stack-name
 #repositoryUri=$(aws ecr create-repository --repository-name poker-analyzer-service --region $region | jq -r '.repository.repositoryUri')
 
 
-aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $repositoryUri
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $repositoryUri
 docker tag claudiocordova/poker-hand-analyzer-microservice:0.0.1-SNAPSHOT $repositoryUri:0.0.1-SNAPSHOT
 docker push $repositoryUri:0.0.1-SNAPSHOT 
 
 
-aws cloudformation delete-stack --region $region --stack-name ecs-task-definition-stack
-aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-task-definition-stack
-aws cloudformation create-stack --region $region --stack-name ecs-task-definition-stack --template-body file://./ecs-task-definition.yaml --parameters ParameterKey=Image,ParameterValue=$repositoryUri:0.0.1-SNAPSHOT   --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation wait stack-create-complete --region $region --stack-name ecs-task-definition-stack
+aws cloudformation delete-stack --region $REGION --stack-name ecs-task-definition-stack
+aws cloudformation wait stack-delete-complete --region $REGION --stack-name ecs-task-definition-stack
+if [ "$MODE" == "FARGATE" ]; then
+  aws cloudformation create-stack --region $REGION --stack-name ecs-task-definition-stack --template-body file://./ecs-task-definition.yaml --parameters ParameterKey=Image,ParameterValue=$repositoryUri:0.0.1-SNAPSHOT   --capabilities CAPABILITY_NAMED_IAM
+elif [ "$MODE" == "EC2" ]; then
+  aws cloudformation create-stack --region $REGION --stack-name ecs-task-definition-stack --template-body file://./ecs-ec2-task-definition.yaml --parameters ParameterKey=Image,ParameterValue=$repositoryUri:0.0.1-SNAPSHOT   --capabilities CAPABILITY_NAMED_IAM
+fi
+aws cloudformation wait stack-create-complete --region $REGION --stack-name ecs-task-definition-stack
