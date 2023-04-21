c7c40bbd4589bb71fe3e54d83cd96227c2cd87d9
Claudio Cordova <claudio_cordova@yahoo.com>
2023-04-21 14:24:45 -0700

    V10

--- src/main/java/com/pokerhandanalyzer/controller/PokerHandController.java
+++ src/main/java/com/pokerhandanalyzer/controller/PokerHandController.java
@@ -32,7 +32,7 @@ public class PokerHandController {
 	
 	@RequestMapping(method=RequestMethod.GET, value ="/test")
 	public PokerHandAnalysis test() {
-		PokerHandAnalysis r = new PokerHandAnalysis("1","hello V9");
+		PokerHandAnalysis r = new PokerHandAnalysis("1","hello V10");
 		return r;
 	}
 	
